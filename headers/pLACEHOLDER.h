a4465e45a4d61fd90881ca6083b8bf0c85cce7ed
Merzlikin AI <97389439+Daidal0s@users.noreply.github.com>
2024-04-04 10:56:54 +0500

    Initial commit

---
+++ headers/pLACEHOLDER.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef pLACEHOLDER_H
+#define pLACEHOLDER_H
+
+class pLACEHOLDER
+{
+public:
+    pLACEHOLDER();
+};
+
+#endif // pLACEHOLDER_H
---
+++ src/pLACEHOLDER.cpp
@@ -0,0 +1,4 @@
+#include <stdafx.h>
+#include "pLACEHOLDER.h"
+
+pLACEHOLDER::pLACEHOLDER() {}
